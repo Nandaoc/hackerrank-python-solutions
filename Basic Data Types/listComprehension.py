02923b6337fcbb3baa064731512d8ea35e546438
Nandaoc <nandaocarvalho11@gmail.com>
2022-04-06 12:07:23 -0300

    data types - list comprehension solution

---
+++ Basic Data Types/listComprehension.py
@@ -0,0 +1,28 @@
+## Solution 1 - list comprehensions solution
+
+if __name__ == '__main__':
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    
+    ar = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k!=n]
+    print(ar)
+
+## Solution 2 - no list comprehensions solution
+
+# if __name__ == '__main__':
+#     x = int(raw_input())
+#     y = int(raw_input())
+#     z = int(raw_input())
+#     n = int(raw_input())
+    
+#     newArr = []
+    
+#     for i in range(x + 1):
+#         for j in range(y + 1):
+#             for k in range(z+1):
+#                 if (i+j+k != n):
+#                     newArr.append([i, j, k])
+#     print(newArr)
+    
